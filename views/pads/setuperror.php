b95853090c81122563d7a1beee694f27cc3cf37e
mlunzena <luniki@gmail.com>
2020-04-22 07:31:20 +0200

    reword client setup error

--- views/pads/setuperror.php
+++ views/pads/setuperror.php
@@ -1,1 +1,7 @@
-<?= MessageBox::error(dgettext('studipad', 'Etherpad Client Setup Fehler')) ?>
+<?= MessageBox::error(
+    dgettext('studipad', 'Etherpad-Konfigurationsfehler'),
+    [
+        dgettext('studipad', 'Die Verbindung zu Etherpad ist nicht möglich.'),
+        dgettext('studipad', 'Wenden Sie sich bitte an ihre Stud.IP-Administration. Das Etherpad-Plugin ist nicht korrekt konfiguriert.'),
+    ]
+) ?>
